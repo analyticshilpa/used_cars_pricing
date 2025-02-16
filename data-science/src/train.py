31618d8f71f73ca45309c4a061471d67d9866f7e
Shilpa Chheda <141972580+analyticshilpa@users.noreply.github.com>
2025-02-16 14:05:39 -0700

    Update train.py
    from pathlib import path

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -10,6 +10,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from pathlib import Path
+
 mlflow.start_run()  # Start the MLflow experiment run
 
 
